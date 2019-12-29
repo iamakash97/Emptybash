d04d78ecf695a5eccaac18194ad8fd51e791b3a1
iamakash <akashmulge@gmail.com>
2019-12-29 13:34:55 +0530

    yes

---
+++ module.js
@@ -0,0 +1,20 @@
+exports.calculate=function(a,b,c)
+{
+    var d=parseInt(a);
+    var e=parseInt(b);
+    var c=parseInt(c);
+
+    var f=((d+e+c)/3);
+    return f;
+    
+}
+exports.multiplication=function(a,b,c)
+{
+    var d=parseInt(a);
+    var e=parseInt(b);
+    var c=parseInt(c);
+
+    var f=(d*e*c);
+    return f;
+    
+}
